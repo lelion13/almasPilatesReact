72444c484aabc12123390d6b38179acdfe1f6420
lelion13 <lelion13@gmail.com>
2024-10-16 16:09:50 -0300

    no anda el submit pero el formulario ya esta con state

--- vite-project/src/pages/Contacto.jsx
+++ vite-project/src/pages/Contacto.jsx
@@ -1,76 +1,96 @@
 import Page from "../Page";
+import "../../public/css/contacto.css";
+import { useState } from "react";
+
 
 function Contacto() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [email, setEmail] = useState("");
+  const [genero, setGenero] = useState("");
+  const [medio, setMedio] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ name, surname, telefono, email, genero, medio, message })
+  };
+
+  const handleChange = (event) => {
+    setGenero(event.target.value);
+  };
+
   return (
     <Page>
       <main className="contacto-main">
         <section id="contacto">
-            <form id="formulario">
-                <label htmlFor="name">Nombre:</label>
-                <input type="text" id="name" name="name" required />
-                <span id="nameError" style={{ color: 'red', margin: '0', padding: '0', fontSize: 'smaller', display: 'none' }}>
-                  Formato de nombre no válido! Debe ingresar solo texto..
-                </span>
-                
-                <label htmlFor="surname">Apellido:</label>
-                <input type="text" id="surname" name="surname" required />
-                <span   id="surnameError"   style={{ color:'red', margin:'0', padding: '0', fontSize: 'smaller', display: 'none' }}>
-                  Formato de apellido no válido! Debe ingresar solo texto..
-                </span>
+          <form id="formulario" onSubmit={handleSubmit}>
+            <label htmlFor="name">Nombre:</label>
+            <input type="text" id="name" name="name" required value={name} onChange={(e) => setName(e.target.value)} />
+            {/* <span id="nameError" style={{ color: 'red', margin: '0', padding: '0', fontSize: 'smaller', display: 'none' }}>
+              Formato de nombre no válido! Debe ingresar solo texto..
+            </span> */}
 
-                <label htmlFor="telefono">Telefono:</label>
-                <input type="tel" id="telefono" name="telefono" pattern="(\d{3}-\d{2}-\d{8})|(\d{10})" required />
-                
-                <label htmlFor="email">Correo Electrónico:</label>
-                <input type="email" id="email" name="email" required />
-                <span id="emailError" style={{ color: 'red', margin: '0', padding: '0', fontSize: 'smaller', display: 'none' }}>Formato de correo electrónico no válido!</span>
+            <label htmlFor="surname">Apellido:</label>
+            <input type="text" id="surname" name="surname" required value={surname} onChange={(e) => setSurname(e.target.value)} />
+            {/* <span id="surnameError" style={{ color: 'red', margin: '0', padding: '0', fontSize: 'smaller', display: 'none' }}>
+              Formato de apellido no válido! Debe ingresar solo texto..
+            </span> */}
 
+            <label htmlFor="telefono">Telefono:</label>
+            <input type="tel" id="telefono" name="telefono" pattern="(\d{3}-\d{2}-\d{8})|(\d{10})" required value={telefono} onChange={(e) => setTelefono(e.target.value)} />
 
-                {/* <fieldset className="genero">
-                    <legend>Indique su genero</legend>
-                    <input type="radio" id="masculino" name="genero" value="masculino">
-                    <label htmlFor="masculino">Masculino</label>
-                    <input type="radio" id="femenino" name="genero" value="femenino">
-                    <label htmlFor="femenino">Femenino</label>
-                </fieldset> */}
-
-                <label htmlFor="medio">Por que medio nos conociste?</label>
-                <select name="medio" id="medio" >
-                    <option value="" disabled selected>Seleccione una opcion</option>
-                    <option value="google">Google</option>
-                    <option value="facebook">Facebook</option>
-                    <option value="instagram">Instagram</option>
-                    <option value="conocido">Conocido</option>
-                    <option value="otro">Otro</option>
-                </select>
+            <label htmlFor="email">Correo Electrónico:</label>
+            <input type="email" id="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+            {/* <span id="emailError" style={{ color: 'red', margin: '0', padding: '0', fontSize: 'smaller', display: 'none' }}>Formato de correo electrónico no válido!</span> */}
 
 
-                <label htmlFor="message">Mensaje:</label>
-                <textarea id="message" name="message" rows="5" required></textarea>
-                
-                <div className="contacto-boton">
-                    <input type="submit" value="Enviar" id="enviar" name="enviar" />
-                    <input type="reset" value="Cancelar" id="cancelar" name="cancelar" />
-                </div>
-                </form>
+            <fieldset className="genero">
+              <legend>Indique su genero</legend>
+              <input type="radio" id="masculino" name="genero" value="masculino" checked={genero === "masculino"} onChange={handleChange} />
+              <label htmlFor="masculino">Masculino</label>
+              <input type="radio" id="femenino" name="genero" value="femenino" checked={genero === "femenino"} onChange={handleChange} />
+              <label htmlFor="femenino">Femenino</label>
+            </fieldset>
+
+            <label htmlFor="medio">Por que medio nos conociste?</label>
+            <select name="medio" id="medio" value={medio} onChange={(e) => setMedio(e.target.value)} >
+              <option value="" disabled selected>Seleccione una opcion</option>
+              <option value="google">Google</option>
+              <option value="facebook">Facebook</option>
+              <option value="instagram">Instagram</option>
+              <option value="conocido">Conocido</option>
+              <option value="otro">Otro</option>
+            </select>
+
+
+            <label htmlFor="message">Mensaje:</label>
+            <textarea id="message" name="message" rows="5" required value={message} onChange={(e) => setMessage(e.target.value)} ></textarea>
+
+            <div className="contacto-boton">
+              <input type="submit" value="Enviar" id="enviar" name="enviar" />
+              <input type="reset" value="Cancelar" id="cancelar" name="cancelar" />
+            </div>
+          </form>
         </section>
         <section id="mapa">
-            <div>
-                <p>Ubicación:</p>
-                <iframe 
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d209647.84173526746!2d-58.61543202933301!3d-34.81229321574116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd10de827944f%3A0x18e6a75e60b143db!2sAlmas%20Movimiento%20Saludable!5e0!3m2!1ses!2sar!4v1721839427802!5m2!1ses!2sar" 
-                  width="400" 
-                  height="300" 
-                  style={{ border: 0 }} 
-                  allowFullScreen="" 
-                  loading="lazy" 
-                  referrerPolicy="no-referrer-when-downgrade">
-                </iframe>
+          <div>
+            <p>Ubicación:</p>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d209647.84173526746!2d-58.61543202933301!3d-34.81229321574116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd10de827944f%3A0x18e6a75e60b143db!2sAlmas%20Movimiento%20Saludable!5e0!3m2!1ses!2sar!4v1721839427802!5m2!1ses!2sar"
+              width="400"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade">
+            </iframe>
 
-            </div>            
-            
+          </div>
+
         </section>
-    </main>
+      </main>
     </Page>
   );
 }
