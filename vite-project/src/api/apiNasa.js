6724153fd2f0b7bd1ad39069684fd4535dd5fccf
lelion13 <lelion13@gmail.com>
2024-10-21 16:50:43 -0300

    Agregando api de nasa, falta consumirlo

---
+++ vite-project/src/api/apiNasa.js
@@ -0,0 +1,21 @@
+async function fetchNasa(count) {
+    try {
+        if (count){
+            const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=icG6JnrcSkQVt5Mn481Nv0yGWaFTZNw5IsI0ZuCA&count=${count}`);
+
+            if (!response.ok){
+                throw new Error("Eror en la solicitud");
+            }
+            const data = await response.json();
+            console.log(data);
+            return data;
+        }
+        return [];
+        
+
+    }catch(error) {
+        console.error(error);
+    };
+}
+
+export default fetchNasa;
